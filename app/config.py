a213b785476e367accb77dc048cbb14232761d4d
Jasontang98 <jasontang@DESKTOP-K3NMI40.localdomain>
2022-07-21 16:13:38 -0700

    config

--- app/config.py
+++ app/config.py
@@ -8,6 +8,10 @@ class Config:
     # (only 'postgresql') but heroku's postgres add-on automatically sets the
     # url in the hidden config vars to start with postgres.
     # so the connection uri must be updated here
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
     SQLALCHEMY_ECHO = True
+
+    S3_BUCKET = os.environ.get("S3_BUCKET_NAME")
+    S3_KEY = os.environ.get("S3_ACCESS_KEY")
+    S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
+    S3_LOCATION = f"http://{S3_BUCKET}.s3.amazonaws.com/"
