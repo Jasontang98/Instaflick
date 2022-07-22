17645c9a9c00dbcdc6c7d2e994165af2218b1e02
Rouben <rouben.ghambaryan@gmail.com>
2022-07-22 13:00:59 -0700

    backend routes for comments

---
+++ app/forms/comment_form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
+
+class CommentForm(FlaskForm):
+    user_id = StringField('User ID', validators=[DataRequired()])
+    image_id = StringField('Image ID', validators=[DataRequired()])
+    comment = TextAreaField('Comment', validators=[DataRequired(), Length(min=1, max=500)])
+    submit = SubmitField('Submit')
