79e306064f090ad01d1699c8b2421997afeb33fd
Rouben <rouben.ghambaryan@gmail.com>
2022-07-22 16:15:53 -0700

    add,delete,get, edit not working

--- react-app/src/components/SingleImage/index.js
+++ react-app/src/components/SingleImage/index.js
@@ -1,153 +1,169 @@
-import { getSingleImage, deleteSingleImage } from "../../store/images";
-// import {
-// 	getCommentsAsync,
-// 	addCommentAsync,
-// 	deleteCommentAsync,
-// 	cleanComments,
-// } from '../../store/comments';
-import { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useParams, useHistory, NavLink, Redirect } from "react-router-dom";
-import "./SingleImage.css";
+import { getSingleImage, deleteSingleImage } from '../../store/images';
+import {
+	getCommentsByImage,
+	deleteAComment,
+	editAComment,
+	addAComment,
+	cleanComments,
+} from '../../store/comments';
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams, useHistory, NavLink, Redirect } from 'react-router-dom';
+import './SingleImage.css';
 
 const SingleImage = () => {
-  const history = useHistory();
-  const dispatch = useDispatch();
-  const { id } = useParams();
-  const oneImage = useSelector((state) => state.images[id]);
-  const sessionUser = useSelector((state) => state.session.user);
+	const history = useHistory();
+	const dispatch = useDispatch();
+	const { id } = useParams();
+	const oneImage = useSelector((state) => state.images[id]);
+	const sessionUser = useSelector((state) => state.session.user);
+	const account = useSelector((state) => state.session.user);
+	const comments = Object.values(useSelector((state) => state.comments));
+	const [comment, setComment] = useState('');
+	const [isLoaded, setLoaded] = useState(false);
+	const [validationErrors, setValidationErrors] = useState([]);
+  console.log(account, '000000000000000')
 
-  const account = useSelector((state) => state.session.user);
-  // const comments = Object.values(useSelector((state) => state.comments));
-  // const [comment, setComment] = useState('');
-  const [isLoaded, setLoaded] = useState(false);
-  const [validationErrors, setValidationErrors] = useState([]);
+	useEffect(() => {
+		// if (!account) history.push('/');
+		dispatch(getSingleImage(id))
+			.then(async () => await dispatch(getCommentsByImage(id)))
+			.then(() => setLoaded(true));
 
-  useEffect(() => {
-    // if (!account) history.push('/');
-    dispatch(getSingleImage(id))
-      // .then(async () => await dispatch(getCommentsAsync(id)))
-      .then(() => setLoaded(true));
+		return () => {
+			dispatch(cleanComments());
+		};
+	}, [id, dispatch]);
 
-    // return () => {
-    // 	dispatch(cleanComments());
-    // };
-  }, [id, dispatch]);
+	const ImageDeleter = async (e) => {
+		await dispatch(deleteSingleImage(id));
+		history.push('/feed');
+	};
 
-  const ImageDeleter = async (e) => {
-    await dispatch(deleteSingleImage(id));
-    history.push("/feed");
-  };
+	if (!oneImage) return null;
 
-  if (!oneImage) return null;
+	const notLoggedIn = () => {
+		if (!account) history.push('/login');
+	};
 
-  const notLoggedIn = () => {
-    if (!account) history.push("/login");
-  };
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setValidationErrors([]);
+		const errors = [];
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setValidationErrors([]);
-    const errors = [];
+		if (comment.length > 500) errors.push('Comment is too long');
+		if (errors.length) {
+			setValidationErrors(errors);
+			return;
+		}
 
-    // if (comment.length > 255) errors.push('Comment is too long');
+		const data = {
+			comment,
+			user_id: account?.id,
+			image_id: oneImage?.id,
+		};
+		await dispatch(addAComment(data))
+			// .then(dispatch(getSingleImage(id)))
+			setComment('');
+	};
 
-    if (errors.length) {
-      setValidationErrors(errors);
-      return;
-    }
+	if (!sessionUser) return <Redirect to="/signup" />;
 
-    const data = {
-      // comment,
-      userId: account?.id,
-      imageId: oneImage?.id,
-    };
-    // dispatch(addCommentAsync(data))
-    // 	.then(dispatch(getSingleImage(id)))
-    // 	.then(setComment(''));
-  };
-
-  if (!sessionUser) return <Redirect to="/signup" />;
-
-  return (
-    isLoaded && (
-      <div className="tester">
-        <div className="images-background-image">
-          <div className="single-image-container3">
-            <h1 className="title">{oneImage.title}</h1>
-            <p className="username">{oneImage?.User?.username}</p>
-            <img
-              alt="uploadedImage"
-              className="single-image-image"
-              src={oneImage.image_url}
-              title={oneImage.title}
-            />
-            <div className="edit-delete-comment-btn">
-              {oneImage?.user_id === account?.id ? (
-                <button className="single-image-submit-btn" type="submit">
-                  <NavLink
-                    className="single-image-submit-btn"
-                    exact
-                    to={`/images/${id}/edit`}
-                  >
-                    Edit
-                  </NavLink>
-                </button>
-              ) : (
-                <></>
-              )}
-              {oneImage?.user_id === account?.id ? (
-                <button
-                  className="single-image-submit-btn"
-                  onClick={ImageDeleter}
-                >
-                  Delete
-                </button>
-              ) : (
-                <></>
-              )}
-            </div>
-            <h3 className="description">{oneImage.description}</h3>
-            {/* <h1 className="comments-single-image">Comments</h1>
+	return (
+		isLoaded && (
+			<div className="tester">
+				<div className="images-background-image">
+					<div className="single-image-container3">
+						<p className="username">{oneImage?.user_id?.username}</p>
+						<img
+							alt="uploadedImage"
+							className="single-image-image"
+							src={oneImage.image_url}
+							title={oneImage.title}
+						/>
+						<div className="edit-delete-comment-btn">
+							{oneImage?.user_id === account?.id ? (
+								<button
+									className="single-image-submit-btn"
+									type="submit"
+								>
+									<NavLink
+										className="single-image-submit-btn"
+										exact
+										to={`/images/${id}/edit`}
+									>
+										Edit
+									</NavLink>
+								</button>
+							) : (
+								<></>
+							)}
+							{oneImage?.user_id === account?.id ? (
+								<button
+									className="single-image-submit-btn"
+									onClick={ImageDeleter}
+								>
+									Delete
+								</button>
+							) : (
+								<></>
+							)}
+						</div>
+						<h3 className="description">{oneImage.description}</h3>
+						<h1 className="comments-single-image">Comments</h1>
 						<div className="comments">
-							{comments &&
-								comments.map((comment) => {
-									return (
-										<div className='new-comment-div' key={comment.id}>
-											<p className="ptagz-comments">
-												{comment?.comment}
-											</p>
-											<p className="ptagz-usename">
-												{comment?.User?.username}
-											</p>
-											<div className='dlt-button-container'>
-												{comment?.User?.username ===
-												account?.username ? (
-													<button
-														className="delete-comment-btn"
-														onClick={() =>
-															dispatch(
-																deleteCommentAsync(
-																	id,
-																	comment.id
-																)
-															).then(() =>
-																dispatch(getCommentsAsync(id))
-															)
-														}
-													>
-														Delete
-													</button>
-												) : (
-													<></>
-												)}
-											</div>
+							{comments.map((comment) => {
+								return (
+									<div className="new-comment-div" key={comment.id}>
+										<p className="ptagz-comments">
+											{comment?.comment}
+										</p>
+										<p className="ptagz-usename">
+											{/* {comment?.user_id} */}
+										</p>
+										<div className="dlt-button-container">
+											{comment?.user_id ===
+											account.id ? (
+												<button
+													className="delete-comment-btn"
+													onClick={() =>
+														dispatch(
+															deleteAComment(id, comment.id)
+														).then(() =>
+															dispatch(getCommentsByImage(id))
+														)
+													}
+												>
+													Delete
+												</button>
+											) : (
+												<></>
+											)}
 										</div>
-									);
-								})}
-						</div> */}
-
-            {/* <form onSubmit={handleSubmit}>
+                    <div className="edit-button-container">
+											{comment?.user_id ===
+											account.id ? (
+												<button
+													className="edit-comment-btn"
+													onClick={() =>
+														dispatch(
+															editAComment(id, comment.id)
+														).then(() =>
+															dispatch(getCommentsByImage(id))
+														)
+													}
+												>
+													Edit
+												</button>
+											) : (
+												<></>
+											)}
+										</div>
+									</div>
+								);
+							})}
+						</div>
+						<form onSubmit={handleSubmit}>
 							<ul>
 								{validationErrors.map((error, idx) => (
 									<li className="errors-signup" key={idx}>
@@ -155,26 +171,26 @@ const SingleImage = () => {
 									</li>
 								))}
 							</ul>
-							{/* <textarea
-								className='comment-text-area'
+							<textarea
+								className="comment-text-area"
 								placeholder="Add a comment"
 								value={comment}
 								required
 								onChange={(e) => setComment(e.target.value)}
-							/> */}
-            {/* <button
+							/>
+							<button
 								className="single-image-submit-btn"
 								onClick={notLoggedIn}
 								type="submit"
 							>
 								Submit
-							</button> */}
-            {/* </form> */}
-          </div>
-        </div>
-      </div>
-    )
-  );
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		)
+	);
 };
 
 export default SingleImage;
