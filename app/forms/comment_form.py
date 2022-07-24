eb7a89495f17ff544ac7fab78ee851a522b5bbc5
Rouben <rouben.ghambaryan@gmail.com>
2022-07-23 19:57:32 -0700

    fixed edit comment

--- app/forms/comment_form.py
+++ app/forms/comment_form.py
@@ -7,3 +7,6 @@ class CommentForm(FlaskForm):
     image_id = StringField('Image ID', validators=[DataRequired()])
     comment = TextAreaField('Comment', validators=[DataRequired(), Length(min=1, max=500)])
     submit = SubmitField('Submit')
+
+class EditCommentForm(FlaskForm):
+    comment = TextAreaField("Comment", validators=[DataRequired(), Length(min=1, max=500)])
