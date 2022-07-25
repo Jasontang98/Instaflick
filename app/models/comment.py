89dfd27adb796fede5e61c7a8ea935a9f428eafd
Exequiel <exequiel.hashimoto@outlook.com>
2022-07-25 15:06:57 -0700

    like relationships established

--- app/models/comment.py
+++ app/models/comment.py
@@ -1,4 +1,5 @@
 from .db import db
+from .comment_like import comment_likes
 
 class Comment(db.Model):
     __tablename__ = 'comments'
@@ -9,6 +10,10 @@ class Comment(db.Model):
     comment = db.Column(db.String(500), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
 
+    user = db.relationship('User', back_populates='comment')
+    image = db.relationship('Image', back_populates='comment')
+    comment_likes = db.relationship('User', secondary=comment_likes, back_populates='user_comments_likes')
+
     def to_dict(self):
         return {
             'id': self.id,
@@ -18,6 +23,11 @@ class Comment(db.Model):
             'created_at': self.created_at
         }
 
-    user = db.relationship('User', back_populates='comment')
-    image = db.relationship('Image', back_populates='comment')
-    comment_like = db.relationship('Comment_Like', back_populates='comment', cascade='all, delete-orphan')
+    # def __repr__(self):
+    #     return '<Comment: {}>'.format(self.id)
+    # @property
+    # def description(self):
+    #     return self.comment
+
+
+    # comment_like = db.relationship('Comment_Like', backref='comment', cascade='all, delete-orphan')
