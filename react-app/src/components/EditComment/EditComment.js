e450273f280cbd504906f3bac4f3f4f72ab4b5cc
Jasontang98 <jasontang@DESKTOP-K3NMI40.localdomain>
2022-07-25 00:29:54 -0700

    last push of the night

--- react-app/src/components/EditComment/EditComment.js
+++ react-app/src/components/EditComment/EditComment.js
@@ -1,62 +1,61 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 
-import { editAComment } from '../../store/comments';
+import { editAComment } from "../../store/comments";
 
-const EditSingleComment = ({ editedComment }) => {
-	console.log(editedComment.comment, '++++ Edited Comment ++++++');
-	const dispatch = useDispatch();
-	const comments = useSelector((state) => state.comments);
-	console.log('this is usesector', comments);
-	const [content, setContent] = useState(editedComment.comment);
+const EditSingleComment = ({ editedComment, setShowModal }) => {
+  const dispatch = useDispatch();
 
-	const [validationErrors, setValidationErrors] = useState([]);
+  const [content, setContent] = useState(editedComment.comment);
 
-	const editSubmit = async (e) => {
-		e.preventDefault();
+  const [validationErrors, setValidationErrors] = useState([]);
 
-		setValidationErrors([]);
-		const errors = [];
+  const editSubmit = async (e) => {
+    e.preventDefault();
 
-		if (content.length > 500) errors.push('Comment is too long');
-		if (errors.length) {
-			setValidationErrors(errors);
-			return;
-		}
-		console.log('this is our content', content);
-		const data = {
-			content,
-			comments: editedComment,
-		};
-		await dispatch(editAComment(data));
-	};
+    setValidationErrors([]);
+    const errors = [];
 
-	return (
-		<div>
-			<div>
-				<div>
-					<h1>Edit Comment</h1>
-					<form onSubmit={editSubmit}>
-						<ul>
-							{validationErrors.map((error, idx) => (
-								<li className="errors-signup" key={idx}>
-									{error}
-								</li>
-							))}
-						</ul>
-						<textarea
-							className="upload-text-area"
-							value={content}
-							onChange={(e) => setContent(e.target.value)}
-						/>
-						<button className="submit-upload" type="submit">
-							Submit
-						</button>
-					</form>
-				</div>
-			</div>
-		</div>
-	);
+    if (content.length > 500) errors.push("Comment is too long");
+    if (errors.length) {
+      setValidationErrors(errors);
+      return;
+    }
+    console.log("this is our content", content);
+    const data = {
+      content,
+      comments: editedComment,
+    };
+    await dispatch(editAComment(data));
+    setShowModal(false);
+  };
+
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Edit Comment</h1>
+          <form onSubmit={editSubmit}>
+            <ul>
+              {validationErrors.map((error, idx) => (
+                <li className="errors-signup" key={idx}>
+                  {error}
+                </li>
+              ))}
+            </ul>
+            <textarea
+              className="upload-text-area"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+            <button className="submit-upload" type="submit">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EditSingleComment;
