bb8ad7aca22642c4fd8a677d8ef9afe9a4515643
Rouben <rouben.ghambaryan@gmail.com>
2022-07-26 09:03:47 -0700

    adding username, db stuff

--- app/forms/comment_form.py
+++ app/forms/comment_form.py
@@ -5,6 +5,7 @@ from wtforms.validators import DataRequired, Length
 class CommentForm(FlaskForm):
     user_id = StringField('User ID', validators=[DataRequired()])
     image_id = StringField('Image ID', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
     comment = TextAreaField('Comment', validators=[DataRequired(), Length(min=1, max=500)])
     submit = SubmitField('Submit')
 
--- app/models/comment.py
+++ app/models/comment.py
@@ -7,6 +7,7 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     image_id = db.Column(db.Integer, db.ForeignKey('images.id'), nullable=False)
+    username = db.Column(db.String(40), nullable=False)
     comment = db.Column(db.String(500), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
 
@@ -19,6 +20,7 @@ class Comment(db.Model):
             'id': self.id,
             'user_id': self.user_id,
             'image_id': self.image_id,
+            'username': self.username,
             'comment': self.comment,
             'created_at': self.created_at
         }
