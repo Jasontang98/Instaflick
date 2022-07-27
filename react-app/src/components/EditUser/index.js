cc2432801d1f2a14c7b257aca97f29d5b9b40e4f
Exequiel <exequiel.hashimoto@outlook.com>
2022-07-27 08:27:58 -0700

    yesterday's work

--- react-app/src/components/EditUser/index.js
+++ react-app/src/components/EditUser/index.js
@@ -1,20 +1,81 @@
-import React, { useState } from "react";
-import { Modal } from "../../context/Modal";
-import EditUser from "./EditUser";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams, useHistory, Redirect, NavLink } from "react-router-dom";
+import { editSingleUser } from "../../store/user"
 
-function EditUserModal() {
-  const [showModal, setShowModal] = useState(false);
+const EditUser = () => {
+    const dispatch = useDispatch();
 
-  return (
-    <>
-      <button onClick={() => setShowModal(true)}>Edit Profile</button>
-      {showModal && (
-        <Modal onClose={() => setShowModal(false)}>
-          <EditUser setShowModal={setShowModal} />
-        </Modal>
-      )}
-    </>
-  );
+    const user = useSelector((state) => state.session.user);
+
+    const [username, setUsername] = useState(user.username);
+    const [description, setDescription] = useState(user.description);
+    const [file, setFile] = useState(user.prof_pic_url);
+    const [validationErrors, setValidationErrors] = useState([]);
+
+    const history = useHistory();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const errors = [];
+
+        // if (description.length > 500) errors.push("Content is too long");
+
+        if (errors.length) {
+            setValidationErrors(errors);
+            return;
+        }
+
+        const data = {
+            description,
+            file,
+            username,
+            id: user?.id
+        }
+
+        dispatch(editSingleUser(data))
+        history.push(`/users/${user.id}`)
+    }
+
+    const handleChange = async (e) => {
+        const uploadFile = e.target.files[0];
+        setFile(uploadFile);
+        };
+
+
+    return (
+        <div>
+            <div>
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        <ul>
+                            {validationErrors.map((error, idx) => (
+                                <li className="errors-signup" key={idx}>
+                                    {error}
+                                </li>
+                            ))}
+                        </ul>
+                        <input
+                        className="username-field"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <textarea
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        />
+                        <input
+                        type="file"
+                        name="profile picture"
+                        onChange={handleChange}
+                        accept=".jpg, .jpeg, .png, .gif, .jfif"
+                        />
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+ );
 }
 
-export default EditUserModal;
+export default EditUser;
