41f9974a5f606c6492d219514ebd25386b11038e
Jasontang98 <jasontang@DESKTOP-K3NMI40.localdomain>
2022-07-29 12:39:32 -0700

    user

--- react-app/src/components/User/index.js
+++ react-app/src/components/User/index.js
@@ -5,7 +5,7 @@ import { getAllImages } from "../../../../react-app/src/store/images.js";
 import "./User.css";
 import { getSingleUser } from "../../store/user.js";
 // import { deleteSingleUser } from "../../store/session.js";
-// import EditUser from "../EditUser";
+import "./User.css";
 
 function User() {
   const dispatch = useDispatch();
@@ -32,42 +32,48 @@ function User() {
   }
 
   return (
-    <>
-      <img src={user.prof_pic_url} alt="profile pic" />
-      <p>{user.description}</p>
-      <ul>
-        <strong>User Id</strong> {userId}
-      </ul>
-      <ul>
-        <strong>{user?.username}</strong>
-        {account.id === parseInt(userId) ? (
-          <div>
-            <NavLink to="/accounts/edit">Edit profile</NavLink>
+    <div className="profileWholeContainer">
+      <div className="info-images">
+        <div className="Topprof">
+          <div className="Profpiccontainer">
+            <img className="profPic" src={user.prof_pic_url} alt="profile pic" />
           </div>
-        ) : (
-          <></>
-        )}
-      </ul>
-      <ul>
-        <strong>Email</strong> {user?.email}
-      </ul>
-      {images.map((image) => (
-        <div key={image.id}>
-          <NavLink exact to={`/images/${image.id}`}>
-            {" "}
-            {parseInt(userId) === image.user_id ? (
-              <img
-                alt="uploaded"
-                className="image-frame"
-                src={image.image_url}
-              />
-            ) : (
-              <></>
-            )}
-          </NavLink>
+          <div className="profDescription">
+            <p>{user.description}</p>
+          </div>
+          <div className="profUsername">
+            <strong>{user?.username}</strong>
+          </div>
+          {account.id === parseInt(userId) ? (
+            <div className="edit-prof-button">
+              <NavLink className="edit-prof" to="/accounts/edit">Edit profile</NavLink>
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
-      ))}{" "}
-    </>
+
+        <div className="imgContainer">
+          {images.map((image) => (
+            <div key={image.id}>
+              <NavLink exact to={`/images/${image.id}`}>
+                {parseInt(userId) === image.user_id ? (
+                  <div className="profImages" >
+                    <img
+                      alt="uploaded"
+                      className="profimage"
+                      src={image.image_url}
+                    />
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </NavLink>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 export default User;
